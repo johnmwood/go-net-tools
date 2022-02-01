b8f78ee904ed63fb6ad93bc5977511c7e59b551b
johnmwood <jmw.home@gmail.com>
2022-01-31 23:26:45 -0600

    changed structure

--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"net"
+	"github.com/johnmwood/net_tools/cmd"
 )
 
-type LocalNetwork struct {
-	WAN_IP string `json: "WAN_IP"`
-}
-
 func main() {
-	file, _ := ioutil.ReadFile("./net_vars.json")
-
-	netVars := LocalNetwork{}
-	_ = json.Unmarshal([]byte(file), &netVars)
-
-	ip := net.ParseIP(netVars.WAN_IP)
-	net.DialIP("tcp", ip)
+	cmd.Execute()
 }
