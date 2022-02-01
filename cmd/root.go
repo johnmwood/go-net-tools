b8d948d39e9e1ff24c95188b36c5d3053a99cb73
johnmwood <jmw.home@gmail.com>
2022-01-31 23:29:06 -0600

    files for cobra

---
+++ cmd/root.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	cfgFile string
+	rootCmd = &cobra.Command{
+		Use:   "nettools",
+		Short: "nettools is a collection of simple networking utilities",
+		Long:  `nettools is a collection of simple networking utilities written in Go with the intent of learning networking concepts and practicing Go`,
+		//		Run: func(cmd *cobra.Command, arg []string)
+	}
+)
+
+func execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.deployer.yaml)")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".deployer")
+	}
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
